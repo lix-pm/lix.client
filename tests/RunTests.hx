3b75a006b9c211b71d41dfddc3304c85046009b1
Juraj Kirchheim <back2dos@googlemail.com>
2018-02-05 12:40:15 +0100

    Travix.

---
+++ tests/RunTests.hx
@@ -0,0 +1,10 @@
+package ;
+
+class RunTests {
+
+  static function main() {
+    travix.Logger.println('it works');
+    travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
+  }
+  
+}
