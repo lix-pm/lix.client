70ea7d47f002c9f4533a64af7380ca0407c426b3
Juraj Kirchheim <back2dos@googlemail.com>
2018-05-03 09:19:08 -0700

    Expand haxelib server api.

--- src/lix/client/sources/haxelib/Repo.hx
+++ src/lix/client/sources/haxelib/Repo.hx
@@ -2,4 +2,27 @@ package lix.client.sources.haxelib;
 
 interface Repo {
   function getLatestVersion(project:String):String;
+  function infos( project : String ) : ProjectInfos;
+}
+
+typedef SemVer = String;
+
+typedef VersionInfos = {
+	var date : String;
+	var name : SemVer;//TODO: this should eventually be called `number`
+	var downloads : Int;
+	var comments : String;
+}
+
+typedef ProjectInfos = {
+	var name : String;
+	var desc : String;
+	var website : String;
+	var owner : String;
+	var contributors : Array<{ name:String, fullname:String }>;
+	var license : String;
+	var curversion : String;
+	var downloads : Int;
+	var versions : Array<VersionInfos>;
+	var tags : List<String>;
 }
