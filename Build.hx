f5cd684fc68a6de0a2df630d29f76beeb0831464
Kevin Leung <kevinresol@gmail.com>
2018-10-02 21:04:51 +0800

    Only noderify js files (skip travix artifacts)

--- Build.hx
+++ Build.hx
@@ -1,12 +1,16 @@
+using haxe.io.Path;
+
 class Build {
   static function main() {
     Sys.command('haxe', ['haxeshim.hxml']);
     Sys.command('haxe', ['lix.cli.hxml']);
     for (file in sys.FileSystem.readDirectory('bin')) {
-      var file = 'bin/$file';
-      var tmp = '$file.bundled';
-      Sys.command('npm run --silent noderify $file > $tmp');
-      sys.FileSystem.rename(tmp, file);
+      if(file.extension() == '.js') {
+        var file = 'bin/$file';
+        var tmp = '$file.bundled';
+        Sys.command('npm run --silent noderify $file > $tmp');
+        sys.FileSystem.rename(tmp, file);
+      }
     }
   }
 }
