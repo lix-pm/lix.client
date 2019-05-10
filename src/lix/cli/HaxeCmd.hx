2d80b1c83b420cc020241a9d7da9ffa7cb45a9ce
Juraj Kirchheim <back2dos@googlemail.com>
2019-05-10 13:31:41 -0700

    Automatically install missing dependencies.

--- src/lix/cli/HaxeCmd.hx
+++ src/lix/cli/HaxeCmd.hx
@@ -14,20 +14,29 @@ class HaxeCmd {
   }
 
   static public function ensure() 
-    return ensureScope().next(function (_) {
-      var hx = new lix.client.haxe.Switcher(
-        Scope.seek(),
-        false,
-        println
-      );
-      return 
-        if (!hx.scope.haxeInstallation.path.exists()) {
+    return ensureScope()
+      .next(function (_) return {
+
+        var scope = Scope.seek();
+        var hx = new lix.client.haxe.Switcher(scope, false, println);
+        
+        (
+          if (!hx.scope.haxeInstallation.path.exists()) {
           var version = hx.scope.config.version;
           println('Version $version configured in ${hx.scope.configFile} does not exist. Attempting download ...');
           hx.install(version, { force: false });
-        }    
-        else Promise.lift(Noise);
-    });
+          }    
+          else Promise.lift(Noise)
+        ).next(function (_) {
+          switch scope.getInstallationInstructions().instructions.install.length {
+            case 0: 
+            case v:
+              println('Missing $v libraries. Attempting download ...');
+              new HaxeCli(scope).installLibs(true);
+          }
+          return Noise;
+        });
+      });
 
   static function main() 
     Command.attempt(ensure(), @:privateAccess HaxeCli.main);
