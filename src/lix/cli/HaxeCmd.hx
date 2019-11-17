22aa5d9eab4d09c8603b250fd9a4dc75de6aac16
Juraj Kirchheim <juraj.kirchheim@doclerholding.com>
2019-11-17 09:42:29 +0100

    Use logger when downloading haxe automatically.

--- src/lix/cli/HaxeCmd.hx
+++ src/lix/cli/HaxeCmd.hx
@@ -2,31 +2,31 @@ package lix.cli;
 
 class HaxeCmd {
   static public function ensureScope():Promise<Noise>
-    return 
+    return
       if (Scope.exists(Scope.DEFAULT_ROOT)) Noise;
-      else 
+      else
         Scope.create(Scope.DEFAULT_ROOT, {
           version: 'stable',
           resolveLibs: Mixed,
-        });      
+        });
 
-  static public function ensure(logger) 
+  static public function ensure(logger)
     return ensureScope()
       .next(function (_) return {
 
         var scope = Scope.seek();
         var hx = new lix.client.haxe.Switcher(scope, logger);
-        
+
         (
           if (!hx.scope.haxeInstallation.path.exists()) {
             var version = hx.scope.config.version;
-            println('Version $version configured in ${hx.scope.configFile} does not exist. Attempting download ...');
+            logger.info('Version $version configured in ${hx.scope.configFile} does not exist. Attempting download ...');
             hx.install(version, { force: false });
-          }    
+          }
           else Promise.lift(Noise)
         ).next(function (_) {
           // switch scope.getInstallationInstructions().instructions.install.length {
-          //   case 0: 
+          //   case 0:
           //   case v:
           //     println('Missing $v libraries. Attempting download ...');
           //     new HaxeCli(scope).installLibs(true);
@@ -35,6 +35,6 @@ class HaxeCmd {
         });
       });
 
-  static function main() 
+  static function main()
     Command.attempt(ensure(Logger.get()), @:privateAccess HaxeCli.main);
 }
